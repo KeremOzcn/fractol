7df360a0cc9da87ce9b74455ea9af84c2b1d625a
Kerem Özcan <139013257+KeremOzcn@users.noreply.github.com>
2025-03-23 16:20:55 +0300

    Add files via upload

---
+++ math.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   math.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kozcan <kozcan@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 15:33:41 by kozcan            #+#    #+#             */
+/*   Updated: 2025/03/21 15:33:43 by kozcan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+t_complex	add_complex(t_complex a, t_complex b)
+{
+	t_complex	sum;
+
+	sum.x = a.x + b.x;
+	sum.y = a.y + b.y;
+	return (sum);
+}
+
+double	abs_val(double x)
+{
+	if (x < 0)
+		x = -x;
+	return (x);
+}
+
+t_complex	square_complex(t_complex z)
+{
+	t_complex	res;
+
+	res.x = (z.x * z.x) - (z.y * z.y);
+	res.y = 2 * z.x * z.y;
+	return (res);
+}
+
+double	mod(t_complex z)
+{
+	double	mod;
+
+	mod = z.x * z.x + z.y * z.y;
+	return (mod);
+}
